0658aab8257daf3d8cae57925fb3d48b188e547c
hrudkouski <aprilshower19@gmail.com>
2021-07-26 18:19:51 +0300

    formik, onSubmit

--- src/n2-features/f1-auth/a2-register/Register.tsx
+++ src/n2-features/f1-auth/a2-register/Register.tsx
@@ -1,21 +1,44 @@
+import {useFormik} from "formik";
+// import { Redirect } from "react-router-dom";
 import SuperInputText from "../../../n1-main/m1-ui/u3-common/Super-Components/c1-SuperInputText/SuperInputText";
 import SuperButton from "../../../n1-main/m1-ui/u3-common/Super-Components/c2-SuperButton/SuperButton";
 
 export const Register = () => {
 
+    // const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
+    // const dispatch = useDispatch();
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            confirmPassword: '',
+        },
+        validate: () => {
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+            formik.resetForm();
+        },
+    });
+
+    // if (isLoggedIn) {
+    //     return <Redirect to={'/'}/>
+    // }
+
     return (
         <div style={{display: 'flex', flexDirection: 'column'}}>
             <span style={{color: 'red'}}>it-incubator</span>
             <span style={{color: 'green'}}>Sign Up</span>
-            <form action="">
+            <form onSubmit={formik.handleSubmit}>
                 <div style={{display: 'flex', flexDirection: 'column'}}>
                     <label htmlFor="email">Email</label>
                     <SuperInputText
                         id="email"
                         name="email"
                         type="text"
-                        // onChange={formik.handleChange}
-                        // value={formik.values.firstName}
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
                     />
                 </div>
                 <div style={{display: 'flex', flexDirection: 'column'}}>
@@ -24,8 +47,8 @@ export const Register = () => {
                         id="password"
                         name="password"
                         type="password"
-                        // onChange={formik.handleChange}
-                        // value={formik.values.firstName}
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
                     />
                 </div>
                 <div style={{display: 'flex', flexDirection: 'column'}}>
@@ -34,12 +57,12 @@ export const Register = () => {
                         id="confirmPassword"
                         name="confirmPassword"
                         type="password"
-                        // onChange={formik.handleChange}
-                        // value={formik.values.firstName}
+                        onChange={formik.handleChange}
+                        value={formik.values.confirmPassword}
                     />
                 </div>
                 <div>
-                    <SuperButton type="submit">Cancel</SuperButton>
+                    <SuperButton type='reset'>Cancel</SuperButton>
                     <SuperButton type="submit">Register</SuperButton>
                 </div>
             </form>
