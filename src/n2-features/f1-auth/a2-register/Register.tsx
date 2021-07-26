bcedf9666d3fc7ccb207c6ee8a04d32ad182081f
hrudkouski <aprilshower19@gmail.com>
2021-07-26 17:49:13 +0300

    creating structure of HTML

--- src/n2-features/f1-auth/a2-register/Register.tsx
+++ src/n2-features/f1-auth/a2-register/Register.tsx
@@ -1,7 +1,48 @@
+import SuperInputText from "../../../n1-main/m1-ui/u3-common/Super-Components/c1-SuperInputText/SuperInputText";
+import SuperButton from "../../../n1-main/m1-ui/u3-common/Super-Components/c2-SuperButton/SuperButton";
+
 export const Register = () => {
+
     return (
-        <div>
-            Hello, I'm a Register component
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+            <span style={{color: 'red'}}>it-incubator</span>
+            <span style={{color: 'green'}}>Sign Up</span>
+            <form action="">
+                <div style={{display: 'flex', flexDirection: 'column'}}>
+                    <label htmlFor="email">Email</label>
+                    <SuperInputText
+                        id="email"
+                        name="email"
+                        type="text"
+                        // onChange={formik.handleChange}
+                        // value={formik.values.firstName}
+                    />
+                </div>
+                <div style={{display: 'flex', flexDirection: 'column'}}>
+                    <label htmlFor="password">Password</label>
+                    <SuperInputText
+                        id="password"
+                        name="password"
+                        type="password"
+                        // onChange={formik.handleChange}
+                        // value={formik.values.firstName}
+                    />
+                </div>
+                <div style={{display: 'flex', flexDirection: 'column'}}>
+                    <label htmlFor="confirmPassword">Confirm password</label>
+                    <SuperInputText
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        type="password"
+                        // onChange={formik.handleChange}
+                        // value={formik.values.firstName}
+                    />
+                </div>
+                <div>
+                    <SuperButton type="submit">Cancel</SuperButton>
+                    <SuperButton type="submit">Register</SuperButton>
+                </div>
+            </form>
         </div>
     )
 }
