d0da95bd6d03d7b52af138954bd3b64b039d13bf
Dior99 <n.shaldaev@yandex.ru>
2021-07-27 12:18:09 +0300

    Added useFormik and data collection from forms

--- src/n2-features/f1-auth/a3-recoveryPassword/RecoveryPassword.tsx
+++ src/n2-features/f1-auth/a3-recoveryPassword/RecoveryPassword.tsx
@@ -1,12 +1,24 @@
 import SuperInputText from "../../../n1-main/m1-ui/u3-common/Super-Components/c1-SuperInputText/SuperInputText"
 import SuperButton from "../../../n1-main/m1-ui/u3-common/Super-Components/c2-SuperButton/SuperButton"
+import {useFormik} from "formik";
 
 export const RecoveryPassword = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+        },
+        onSubmit: values => {
+            console.log(values)
+        },
+    });
+
     return (
-        <form>
+        <form onSubmit={formik.handleSubmit}>
             <div>
                 <label htmlFor="email">Email Address</label>
-                <SuperInputText type="email"/>
+                <SuperInputText type="email"
+                                {...formik.getFieldProps("email")}/>
             </div>
             <SuperButton type="submit">Submit</SuperButton>
         </form>
--- src/n2-features/f1-auth/a4-new-password/NewPassword.tsx
+++ src/n2-features/f1-auth/a4-new-password/NewPassword.tsx
@@ -1,16 +1,30 @@
 import SuperInputText from "../../../n1-main/m1-ui/u3-common/Super-Components/c1-SuperInputText/SuperInputText";
 import SuperButton from "../../../n1-main/m1-ui/u3-common/Super-Components/c2-SuperButton/SuperButton";
+import {useFormik} from "formik";
 
 export const NewPassword = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            newPassword: '',
+            repeatPassword: ''
+        },
+        onSubmit: values => {
+            console.log(values)
+        },
+    });
+
     return (
-        <form>
+        <form onSubmit={formik.handleSubmit}>
             <div>
                 <label htmlFor="password">New Password</label>
-                <SuperInputText type="password"/>
+                <SuperInputText type="password"
+                                {...formik.getFieldProps("newPassword")}/>
             </div>
             <div>
                 <label htmlFor="password">Repeat Password</label>
-                <SuperInputText type="password"/>
+                <SuperInputText type="password"
+                                {...formik.getFieldProps("repeatPassword")}/>
             </div>
             <SuperButton type="submit">Submit</SuperButton>
         </form>
