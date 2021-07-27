4d89a13e781fd5755453e149b25c47d9edbead42
Dior99 <n.shaldaev@yandex.ru>
2021-07-27 11:53:37 +0300

    Added UI for components RecoveryPassword and NewPassword

--- src/n2-features/f1-auth/a3-recoveryPassword/RecoveryPassword.tsx
+++ src/n2-features/f1-auth/a3-recoveryPassword/RecoveryPassword.tsx
@@ -1,7 +1,14 @@
+import SuperInputText from "../../../n1-main/m1-ui/u3-common/Super-Components/c1-SuperInputText/SuperInputText"
+import SuperButton from "../../../n1-main/m1-ui/u3-common/Super-Components/c2-SuperButton/SuperButton"
+
 export const RecoveryPassword = () => {
     return (
-        <>
-            Recovery Password Page
-        </>
+        <form>
+            <div>
+                <label htmlFor="email">Email Address</label>
+                <SuperInputText type="email"/>
+            </div>
+            <SuperButton type="submit">Submit</SuperButton>
+        </form>
     )
 }
--- src/n2-features/f1-auth/a4-new-password/NewPassword.tsx
+++ src/n2-features/f1-auth/a4-new-password/NewPassword.tsx
@@ -1,7 +1,18 @@
+import SuperInputText from "../../../n1-main/m1-ui/u3-common/Super-Components/c1-SuperInputText/SuperInputText";
+import SuperButton from "../../../n1-main/m1-ui/u3-common/Super-Components/c2-SuperButton/SuperButton";
+
 export const NewPassword = () => {
     return (
-        <>
-            New Password Page
-        </>
+        <form>
+            <div>
+                <label htmlFor="password">New Password</label>
+                <SuperInputText type="password"/>
+            </div>
+            <div>
+                <label htmlFor="password">Repeat Password</label>
+                <SuperInputText type="password"/>
+            </div>
+            <SuperButton type="submit">Submit</SuperButton>
+        </form>
     )
 }
