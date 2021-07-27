e1b21fecaccdbae4b1f68ee9e76543de6523aab0
Dior99 <n.shaldaev@yandex.ru>
2021-07-27 12:50:26 +0300

    Added validation for forms

--- src/n2-features/f1-auth/a3-recoveryPassword/RecoveryPassword.tsx
+++ src/n2-features/f1-auth/a3-recoveryPassword/RecoveryPassword.tsx
@@ -2,12 +2,29 @@ import SuperInputText from "../../../n1-main/m1-ui/u3-common/Super-Components/c1
 import SuperButton from "../../../n1-main/m1-ui/u3-common/Super-Components/c2-SuperButton/SuperButton"
 import {useFormik} from "formik";
 
+type ErrorsDataType = {
+    email?: string
+}
+
 export const RecoveryPassword = () => {
 
     const formik = useFormik({
         initialValues: {
             email: '',
         },
+
+        validate: (values => {
+            let errors: ErrorsDataType = {};
+
+            if (!values.email) {
+                errors.email = "Required"
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address'
+            }
+
+            return errors;
+        }),
+
         onSubmit: values => {
             console.log(values)
         },
@@ -19,6 +36,9 @@ export const RecoveryPassword = () => {
                 <label htmlFor="email">Email Address</label>
                 <SuperInputText type="email"
                                 {...formik.getFieldProps("email")}/>
+                {formik.errors.email
+                    ? (<div style={{color: "red"}}>{formik.errors.email}</div>)
+                    : null}
             </div>
             <SuperButton type="submit">Submit</SuperButton>
         </form>
--- src/n2-features/f1-auth/a4-new-password/NewPassword.tsx
+++ src/n2-features/f1-auth/a4-new-password/NewPassword.tsx
@@ -2,6 +2,11 @@ import SuperInputText from "../../../n1-main/m1-ui/u3-common/Super-Components/c1
 import SuperButton from "../../../n1-main/m1-ui/u3-common/Super-Components/c2-SuperButton/SuperButton";
 import {useFormik} from "formik";
 
+type ErrorsDataType = {
+    newPassword?: string
+    repeatPassword?: string
+}
+
 export const NewPassword = () => {
 
     const formik = useFormik({
@@ -9,6 +14,25 @@ export const NewPassword = () => {
             newPassword: '',
             repeatPassword: ''
         },
+
+        validate: (values => {
+            let errors: ErrorsDataType = {};
+
+            if (!values.newPassword) {
+                errors.newPassword = "Required"
+            } else if (values.newPassword.length < 8) {
+                errors.newPassword = "Password must be more than 7 characters."
+            }
+
+            if (!values.repeatPassword) {
+                errors.repeatPassword = "Required"
+            } else if (values.repeatPassword !== values.newPassword) {
+                errors.repeatPassword = "Password mismatch"
+            }
+
+            return errors;
+        }),
+
         onSubmit: values => {
             console.log(values)
         },
@@ -20,11 +44,17 @@ export const NewPassword = () => {
                 <label htmlFor="password">New Password</label>
                 <SuperInputText type="password"
                                 {...formik.getFieldProps("newPassword")}/>
+                {formik.touched.newPassword && formik.errors.newPassword
+                    ? (<div style={{color: "red"}}>{formik.errors.newPassword}</div>)
+                    : null}
             </div>
             <div>
                 <label htmlFor="password">Repeat Password</label>
                 <SuperInputText type="password"
                                 {...formik.getFieldProps("repeatPassword")}/>
+                {formik.touched.repeatPassword && formik.errors.repeatPassword
+                    ? (<div style={{color: "red"}}>{formik.errors.repeatPassword}</div>)
+                    : null}
             </div>
             <SuperButton type="submit">Submit</SuperButton>
         </form>
