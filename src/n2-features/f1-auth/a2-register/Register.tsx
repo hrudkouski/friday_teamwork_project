c8ec5beec3f16fdc8a54794fb6074ced11c28877
hrudkouski <aprilshower19@gmail.com>
2021-07-28 12:55:43 +0300

    validation registration form, refactoring SuperInputText

--- src/n2-features/f1-auth/a2-register/Register.tsx
+++ src/n2-features/f1-auth/a2-register/Register.tsx
@@ -6,6 +6,12 @@ import {useDispatch, useSelector} from "react-redux";
 import {registerUser} from "./register-reducer";
 import {AppRootStateType} from "../../../n1-main/m2-bll/store/redux-store";
 
+type FormikErrorType = {
+    email?: string
+    password?: string
+    confirmPassword?: string
+}
+
 export const Register = () => {
 
     const isRegister = useSelector<AppRootStateType, boolean>(state => state.register.isRegister)
@@ -17,10 +23,33 @@ export const Register = () => {
             password: '',
             confirmPassword: '',
         },
-        validate: () => {
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Email is required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+
+            if (!values.password) {
+                errors.password = 'Password is required';
+            } else if (!/(?=.*[0-9])/gi.test(values.password)) {
+                errors.password = 'The password must contain at least one number'
+            } else if (!/(?=.*[a-z])(?=.*[A-Z])/gi.test(values.password)) {
+                errors.password = 'The password  must contain at least one lowercase or uppercase Latin letter'
+            } else if (!/[0-9a-zA-Z!@#$%^&*]{8,}/gi.test(values.password)) {
+                errors.password = 'The password must have a minimum 8 characters'
+            }
+
+            if (!values.confirmPassword) {
+                errors.confirmPassword = 'Confirm password is required';
+            } else if (values.password !== values.confirmPassword) {
+                errors.confirmPassword = 'The password and confirm password fields do not match.';
+            }
+
+            return errors;
         },
         onSubmit: values => {
-            // alert(JSON.stringify(values, null, 2));
             dispatch(registerUser(values.email, values.password))
             formik.resetForm();
         },
@@ -39,31 +68,31 @@ export const Register = () => {
                     <label htmlFor="email">Email</label>
                     <SuperInputText
                         id="email"
-                        name="email"
-                        type="text"
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
+                        type="email"
+                        {...formik.getFieldProps('email')}
                     />
+                    {formik.errors.email && formik.touched.email &&
+                    <div style={{color: 'red'}}>{formik.errors.email}</div>}
                 </div>
                 <div style={{display: 'flex', flexDirection: 'column'}}>
                     <label htmlFor="password">Password</label>
                     <SuperInputText
                         id="password"
-                        name="password"
                         type="password"
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
+                        {...formik.getFieldProps('password')}
                     />
+                    {formik.errors.password && formik.touched.password &&
+                    <div style={{color: 'red', width: '300px'}}>{formik.errors.password}</div>}
                 </div>
                 <div style={{display: 'flex', flexDirection: 'column'}}>
                     <label htmlFor="confirmPassword">Confirm password</label>
                     <SuperInputText
                         id="confirmPassword"
-                        name="confirmPassword"
                         type="password"
-                        onChange={formik.handleChange}
-                        value={formik.values.confirmPassword}
+                        {...formik.getFieldProps('confirmPassword')}
                     />
+                    {formik.errors.confirmPassword && formik.touched.confirmPassword &&
+                    <div style={{color: 'red', width: '300px'}}>{formik.errors.confirmPassword}</div>}
                 </div>
                 <div>
                     <SuperButton type='reset'>Cancel</SuperButton>
@@ -73,5 +102,3 @@ export const Register = () => {
         </div>
     )
 }
-
-// My first commit
